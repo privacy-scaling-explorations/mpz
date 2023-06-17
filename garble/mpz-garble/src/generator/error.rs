9557c9302704af3bffaa086ae93c0aa5f438be67
sinu <65924192+sinui0@users.noreply.github.com>
2023-06-16 19:57:47 -0700

    rename crates to mpz

---
+++ garble/mpz-garble/src/generator/error.rs
@@ -0,0 +1,27 @@
+use mpz_core::value::ValueRef;
+use mpz_garble_core::ValueError;
+
+/// Errors that can occur while performing the role of a generator
+#[derive(Debug, thiserror::Error)]
+#[allow(missing_docs)]
+pub enum GeneratorError {
+    #[error(transparent)]
+    CoreError(#[from] mpz_garble_core::GeneratorError),
+    // TODO: Fix the size of this error
+    #[error(transparent)]
+    OTError(Box<mpz_ot::OTError>),
+    #[error(transparent)]
+    IOError(#[from] std::io::Error),
+    #[error(transparent)]
+    ValueError(#[from] ValueError),
+    #[error("missing encoding for value")]
+    MissingEncoding(ValueRef),
+    #[error(transparent)]
+    EncodingRegistryError(#[from] crate::registry::EncodingRegistryError),
+}
+
+impl From<mpz_ot::OTError> for GeneratorError {
+    fn from(err: mpz_ot::OTError) -> Self {
+        Self::OTError(Box::new(err))
+    }
+}
