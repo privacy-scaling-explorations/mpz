c8394d47093dc53818f22cb9bcbee564f2a23f4a
th4s <th4s@metavoid.xyz>
2023-09-27 05:24:08 +0200

    Add garble bench for sha256

--- garble/mpz-garble-core/benches/garble.rs
+++ garble/mpz-garble-core/benches/garble.rs
@@ -1,5 +1,5 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use mpz_circuits::circuits::AES128;
+use mpz_circuits::circuits::{build_sha256, AES128};
 use mpz_garble_core::{ChaChaEncoder, Encoder, Generator};
 
 fn criterion_benchmark(c: &mut Criterion) {
@@ -31,6 +31,29 @@ fn criterion_benchmark(c: &mut Criterion) {
                 Generator::new_with_hasher(aes_ref.into_iter(), encoder.delta(), &inputs).unwrap();
 
             let mut enc_gates = Vec::with_capacity(AES128.and_count());
+            for gate in gen.by_ref() {
+                enc_gates.push(gate);
+            }
+
+            black_box(gen.outputs().unwrap())
+        })
+    });
+
+    let length = 512;
+    let sha256 = build_sha256(0, length);
+
+    let inputs = sha256
+        .inputs()
+        .iter()
+        .map(|value| encoder.encode_by_type(0, &value.value_type()))
+        .collect::<Vec<_>>();
+
+    group.bench_function(format!("sha256_with_length_{}", length), |b| {
+        b.iter(|| {
+            let mut gen =
+                Generator::new_with_hasher(sha256.into_iter(), encoder.delta(), &inputs).unwrap();
+
+            let mut enc_gates = Vec::with_capacity(sha256.and_count());
             for gate in gen.by_ref() {
                 enc_gates.push(gate);
             }
