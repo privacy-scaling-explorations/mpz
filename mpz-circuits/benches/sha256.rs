3cfea4656190767af40c23dfb37345ff379879f3
th4s <th4s@metavoid.xyz>
2023-10-12 01:34:03 +0200

    Repair bench

--- mpz-circuits/benches/sha256.rs
+++ mpz-circuits/benches/sha256.rs
@@ -1,5 +1,6 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use mpz_circuits::{circuits::build_sha256, types::Value};
+use std::sync::Arc;
 
 fn criterion_benchmark(c: &mut Criterion) {
     let length = 512;
@@ -8,11 +9,11 @@ fn criterion_benchmark(c: &mut Criterion) {
         bench.iter(|| black_box(build_sha256(0, length)))
     });
 
-    let sha256 = build_sha256(0, length);
+    let sha256 = Arc::new(build_sha256(0, length));
     c.bench_function("compute_sha256", |bench| {
         bench.iter(|| {
             black_box(
-                sha256
+                Arc::clone(&sha256)
                     .evaluate(&[
                         Value::Array(vec![Value::U32(0); 8]),
                         Value::Array(vec![Value::U8(0); length]),
