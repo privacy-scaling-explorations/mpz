499b75d20397235f8399817a53c65fa2d399d3b8
sinu.eth <65924192+sinui0@users.noreply.github.com>
2024-01-17 11:46:35 -0800

    Random correlated OT traits + ideal impls (#95)
    * feat: random correlated OT traits + ideal impls
    
    * Apply suggestions from code review
    
    Co-authored-by: dan <themighty1@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: dan <themighty1@users.noreply.github.com>

--- ot/mpz-ot/src/ideal/owned/mod.rs
+++ ot/mpz-ot/src/ideal/owned/mod.rs
@@ -1,5 +1,7 @@
 mod cot;
 mod ot;
+mod rcot;
 
 pub use cot::{ideal_cot_pair, IdealCOTReceiver, IdealCOTSender};
 pub use ot::{ideal_ot_pair, IdealOTReceiver, IdealOTSender};
+pub use rcot::{ideal_random_cot_pair, IdealRandomCOTReceiver, IdealRandomCOTSender};
--- ot/mpz-ot/src/ideal/shared/mod.rs
+++ ot/mpz-ot/src/ideal/shared/mod.rs
@@ -1,5 +1,9 @@
 mod cot;
 mod ot;
+mod rcot;
 
 pub use cot::{ideal_cot_shared_pair, IdealSharedCOTReceiver, IdealSharedCOTSender};
 pub use ot::{ideal_ot_shared_pair, IdealSharedOTReceiver, IdealSharedOTSender};
+pub use rcot::{
+    ideal_random_cot_shared_pair, IdealSharedRandomCOTReceiver, IdealSharedRandomCOTSender,
+};
