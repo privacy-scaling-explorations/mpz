9b5efd0d57d8cc2e0c9034852774ed550d6b20ea
th4s <th4s@metavoid.xyz>
2024-02-19 18:05:50 +0100

    Added OLE implementation in mpz-ole-core

--- ole/mpz-ole-core/src/ole/role/provider.rs
+++ ole/mpz-ole-core/src/ole/role/provider.rs
@@ -1,2 +1,84 @@
+use mpz_share_conversion_core::Field;
+use std::marker::PhantomData;
+
+use crate::OLECoreError;
+
 /// A provider for OLE with errors
-pub struct OLEeProvider;
+pub struct OLEeProvider<F>(PhantomData<F>);
+
+impl<F: Field> OLEeProvider<F> {
+    /// Creates a new [`OLEeProvider`].
+    pub fn new() -> Self {
+        OLEeProvider(PhantomData)
+    }
+
+    /// Masks the OLEe input with the ROLEe input
+    ///
+    /// # Arguments
+    ///
+    /// * `ak_dash` - The ROLEe input factors
+    /// * `ak` - The chosen OLEe input
+    ///
+    /// # Returns
+    ///
+    /// * `uk` - The masked chosen input factors, which will be sent to the evaluator
+    pub fn create_mask(&self, ak_dash: &[F], ak: &[F]) -> Result<Vec<F>, OLECoreError> {
+        if ak_dash.len() != ak.len() {
+            return Err(OLECoreError::LengthMismatch(format!(
+                "Number of ROLE inputs {} does not match number of OLE inputs {}.",
+                ak_dash.len(),
+                ak.len(),
+            )));
+        }
+
+        let uk: Vec<F> = ak_dash
+            .iter()
+            .zip(ak.iter().copied())
+            .map(|(&f, a)| a + f)
+            .collect();
+
+        Ok(uk)
+    }
+
+    /// Generates the OLEe output
+    ///
+    /// # Arguments
+    ///
+    /// * `ak_dash` - The ROLEe input
+    /// * `xk_dash` - The ROLEe output
+    /// * `vk` - The masked chosen input factors from the evaluator
+    ///
+    /// # Returns
+    ///
+    /// * `xk` - The OLEe output for the provider
+    pub fn generate_output(
+        &self,
+        ak_dash: &[F],
+        xk_dash: &[F],
+        vk: &[F],
+    ) -> Result<Vec<F>, OLECoreError> {
+        if ak_dash.len() != xk_dash.len() || xk_dash.len() != vk.len() {
+            return Err(OLECoreError::LengthMismatch(format!(
+                "Length of field element vectors does not match. ak: {}, xk_dash: {}, vk: {}",
+                ak_dash.len(),
+                xk_dash.len(),
+                vk.len(),
+            )));
+        }
+
+        let xk: Vec<F> = xk_dash
+            .iter()
+            .zip(ak_dash.iter().copied())
+            .zip(vk.iter().copied())
+            .map(|((&x, a), v)| -(-x + -a * v))
+            .collect();
+
+        Ok(xk)
+    }
+}
+
+impl<F: Field> Default for OLEeProvider<F> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
--- ole/mpz-ole/src/ole/role/provider.rs
+++ ole/mpz-ole/src/ole/role/provider.rs
@@ -6,8 +6,8 @@ use crate::{
 use async_trait::async_trait;
 use futures::SinkExt;
 use mpz_core::ProtocolMessage;
+use mpz_ole_core::ole::role::OLEeProvider as OLEeCoreProvider;
 use mpz_share_conversion_core::Field;
-use std::marker::PhantomData;
 use utils_aio::{
     sink::IoSink,
     stream::{ExpectStreamExt, IoStream},
@@ -16,7 +16,7 @@ use utils_aio::{
 /// A provider for various OLE constructions.
 pub struct OLEeProvider<const N: usize, T: RandomOLEeProvide<F>, F: Field> {
     role_provider: T,
-    field: PhantomData<F>,
+    ole_core: OLEeCoreProvider<F>,
 }
 
 impl<const N: usize, T: RandomOLEeProvide<F>, F: Field> OLEeProvider<N, T, F> {
@@ -27,7 +27,7 @@ impl<const N: usize, T: RandomOLEeProvide<F>, F: Field> OLEeProvider<N, T, F> {
 
         Self {
             role_provider,
-            field: PhantomData,
+            ole_core: OLEeCoreProvider::default(),
         }
     }
 }
@@ -51,7 +51,7 @@ where
         stream: &mut St,
         factors: Vec<F>,
     ) -> Result<Vec<F>, OLEError> {
-        let (ak, xk) = self
+        let (ak_dash, xk_dash) = self
             .role_provider
             .provide_random(
                 &mut into_role_sink(sink),
@@ -60,26 +60,21 @@ where
             )
             .await?;
 
-        let uk: Vec<F> = factors
-            .iter()
-            .zip(ak.iter().copied())
-            .map(|(&f, a)| a + f)
-            .collect();
+        let uk: Vec<F> = self.ole_core.create_mask(&ak_dash, &factors)?;
 
         sink.send(OLEeMessage::ProviderDerand(uk)).await?;
+
         let vk: Vec<F> = stream
             .expect_next()
             .await?
             .try_into_evaluator_derand()
             .map_err(|err| OLEError::WrongMessage(err.to_string()))?;
 
-        let alpha_k: Vec<F> = xk
-            .iter()
-            .zip(ak)
-            .zip(vk)
-            .map(|((&x, a), v)| -(-x + -a * v))
-            .collect();
+        let x_k: Vec<F> = self
+            .ole_core
+            .generate_output(&ak_dash, &xk_dash, &vk)
+            .unwrap();
 
-        Ok(alpha_k)
+        Ok(x_k)
     }
 }
